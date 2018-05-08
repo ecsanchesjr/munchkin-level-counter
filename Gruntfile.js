f31cef4a15800265c092e16f4bb0a7d779358cbf
ecsanchesjr <ecsanchesjr@gmail.com>
2018-05-08 20:20:33 +0000

    Configs

--- Gruntfile.js
+++ Gruntfile.js
@@ -6,44 +6,11 @@ module.exports = function (grunt) {
                style: 'expanded',
                sourcemap: 'none'
             },
-
-            // files: [{
-            //    expand: true,
-            //    cwd: assets+"/css/scss",
-            //    src: '*.scss',
-            //    dest: assets+"/css/",
-            //    ext: '.css'
-            // }]
             files: {
-               'resources/styles/style.css': 'resources/styles/sass/style.scss'
+              'resources/styles/style.css': 'resources/styles/sass/style.scss'
             }
          }
       },
-
-      // sass_globbing: {
-      //    your_target: {
-      //       files: {
-      //          'wp-content/themes/clik/assets/css/scss/allImports.scss': assets + '/css/scss/*.scss',
-      //       },
-      //       options: {
-      //          useSingleQuotes: false,
-      //          signature: '// Hello, World!'
-      //       }
-      //    }
-      // },
-
-      // cssmin: {
-      //    options: {
-      //       mergeIntoShorthands: false,
-      //       roundingPrecision: -1
-      //    },
-      //    target: {
-      //       files: {
-      //          'wp-content/themes/clik/assets/css/clikStyle.min.css': assets + "/css/allImports.css"
-      //       }
-      //    }
-      // },
-
       postcss: {
         options: {
           map: true,
@@ -55,24 +22,8 @@ module.exports = function (grunt) {
           src: "resources/styles/style.css"
         }
       },
-/*
-      uglify: {
-         my_target: {
-            files: {
-				src: 'wp-content/themes/clik/assets/js/*.js',
-				dest: 'wp-content/themes/clik/assets/js/base.js',
-            }
-         }
-      },*/
 
       watch: {
-        /* scripts: {
-           files: assets+"js/*.js",
-           tasks: ['uglify'],
-           options: {
-             spawn: false,
-           },
-         },*/
          css: {
             files: "resources/styles/sass/*.scss",
             tasks: [ 'sass' , 'postcss'],
@@ -81,9 +32,6 @@ module.exports = function (grunt) {
    });
 
    grunt.loadNpmTasks('grunt-contrib-sass');
-   //grunt.loadNpmTasks('grunt-sass-globbing');
-   //grunt.loadNpmTasks('grunt-contrib-cssmin');
-   //grunt.loadNpmTasks('grunt-contrib-uglify');
    grunt.loadNpmTasks('grunt-contrib-watch');
    grunt.loadNpmTasks('grunt-postcss');
 
